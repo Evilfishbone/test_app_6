296182c8795db46308a73895d8d88e9d5216cf45
Leo Lee <nope_not_here@yahoo.com>
2022-04-10 11:27:27 +0000

    Add user edit action and temple

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
@@ -13,7 +15,22 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Profile was updated successfully."
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+  
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
   
   def user_params
     params.require(:user).permit(:username, :email, :password)
