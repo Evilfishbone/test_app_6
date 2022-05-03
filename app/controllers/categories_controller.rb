bd3dc4922d4f660c1eae92848a0b68e089963ad7
Leo Lee <nope_not_here@yahoo.com>
2022-05-03 18:11:27 +0000

    Add edit function for category names

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,6 +14,20 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
+    
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated succssfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
   
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -23,6 +37,7 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
+
   
   private
   
